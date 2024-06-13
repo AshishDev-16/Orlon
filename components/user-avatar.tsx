e126c75e637e85cd81cfd73b1d8614767ebdd425
AshishKadu <kaduashish15@gmail.com>
2024-06-13 14:24:26 +0530

    avatar-image

--- components/user-avatar.tsx
+++ components/user-avatar.tsx
@@ -6,7 +6,7 @@ export const UserAvatar = () => {
 const { user } =useUser();
     return ( 
         <Avatar className="h-8 w-8">
-            <AvatarImage src={user?.profileImageUrl}/>
+            <AvatarImage src=""/>
             <AvatarFallback>
                 {user?.firstName?.charAt(0)}
                 {user?.lastName?.charAt(0)}
